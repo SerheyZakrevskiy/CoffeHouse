5cad0ef7248c094afd1f05cf90339bad6d2f7a0b
Serhiy <serhey781@gmail.com>
2023-11-21 07:10:24 +0200

    feat: add adaptive on site,create burger menu for max-width: 768px

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const iconBurger = document.querySelector('.burger-icon')
+const burgerMenu = document.querySelector('.burger-menu')
+const linksBurger = document.querySelectorAll('.nav-li-a-burger')
+
+iconBurger.addEventListener('click', (e) => {
+    iconBurger.classList.toggle('_active')
+    burgerMenu.classList.toggle('_active')
+    document.body.classList.toggle('_lock')
+})
+
+
+
+linksBurger.forEach(link => {
+    link.addEventListener('click', (e) => {
+        if (iconBurger.classList.contains('_active')) {
+            iconBurger.classList.remove('_active')
+            burgerMenu.classList.remove('_active')
+            document.body.classList.remove('_lock')
+        }
+    });
+});
