ae9f06fe49551dd2ee05335840c94508d37e5f4a
Serhiy <serhey781@gmail.com>
2023-11-24 06:51:07 +0200

    feat slider on section favorites coffee and add some functional on navigation block

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Burger memu
 const iconBurger = document.querySelector('.burger-icon')
 const burgerMenu = document.querySelector('.burger-menu')
 const linksBurger = document.querySelectorAll('.nav-li-a-burger')
@@ -16,4 +17,43 @@ linksBurger.forEach(link => {
             document.body.classList.remove('_lock')
         }
     });
+});
+
+//  Slider favorites coffee
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".content-slider-favourites-coffee-swipe");
+    const dots = document.querySelectorAll(".dot");
+    let currentSlideIndex = 0;
+
+    function showSlide(index) {
+        if (index === currentSlideIndex) return;
+
+        slides[currentSlideIndex].classList.add("hidden");
+        slides[index].classList.remove("hidden");
+
+        dots.forEach((dot, dotIndex) => {
+            dot.classList.toggle("active", dotIndex === index);
+        });
+
+        currentSlideIndex = index;
+    }
+
+    function nextSlide() {
+        const nextIndex = (currentSlideIndex + 1) % slides.length;
+        showSlide(nextIndex);
+    }
+
+    function prevSlide() {
+        const prevIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+        showSlide(prevIndex);
+    }
+
+    document.querySelector(".buttons-left-favourites-coffee").addEventListener("click", prevSlide);
+    document.querySelector(".buttons-right-favourites-coffee").addEventListener("click", nextSlide);
+
+    // Опционально: Автоматическое слайд-шоу
+    // setInterval(nextSlide, 5000);
+
+    showSlide(currentSlideIndex);
 });
